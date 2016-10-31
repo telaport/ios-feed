c56cfcca8f9589db9e0f14f1de0db7a1d1c16e01
Gregory Zarski <gregory.zarski@gmail.com>
2016-10-30 21:13:26 -0700

    Initial commit

---
+++ Feed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Feed
+//
+//  Created by Gregory Zarski on 10/28/16.
+//  Copyright © 2016 Gregory Zarski. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Feed' do
+  use_frameworks!
+
+  # Pods for Feed
+  pod 'SwiftyJSON', '~> 3.1'
+  pod 'Mapbox-iOS-SDK', '~> 3.3'
+  pod 'Alamofire', '~> 4.0'
+end
